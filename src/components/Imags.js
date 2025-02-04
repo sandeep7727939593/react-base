227424d54787a4c316344aebbdf6d7515c7d690f
Sandeep Kumar Jangir <sandeep7727@gmail.com>
2025-02-04 09:32:03 +0530

    master

--- src/components/Imags.js
+++ src/components/Imags.js
@@ -1,6 +1,6 @@
 export default function Images() {
 
     return <div className="image">
-            <img  width="300px" src="./logo512.png"></img>
+            <img  width="300px" src="./logo512.png" alt="image"></img>
         </div>
 }
